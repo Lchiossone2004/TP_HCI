664b12d08e3c6111f7d02f1df1beb490e57b06a2
Martina <mnudel@itba.edu.ar>
2025-05-09 15:54:26 -0300

    Revert "Merge remote-tracking branch 'origin/profile-page'"
    This reverts commit 9b5b7fe76acb031ed5147ac38fff5fba9f3f161c, reversing
    changes made to 262eb0e74610fc99c223ae95d2a330800ca2be6c.

--- tp-hci-g3/src/router.js
+++ tp-hci-g3/src/router.js
@@ -1,19 +1,13 @@
 // src/router/index.js
 import { createRouter, createWebHistory } from 'vue-router'
 import HomePage from './pages/HomePage.vue' 
-import ProfilePage from './pages/ProfilePage'
 
 const routes = [
-  // {
-  //   path: '/',
-  //   name: 'Home',
-  //   component: HomePage
-  // },
   {
     path: '/',
-    name: 'ProfilePage',
-    component: ProfilePage
-  }
+    name: 'Home',
+    component: HomePage
+  },
   
 ]
 
