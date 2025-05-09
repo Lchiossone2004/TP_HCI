fa8d36aee92df2b50e7c47ea96bfbf60e5ab70f8
Martina <mnudel@itba.edu.ar>
2025-05-09 16:00:45 -0300

    profile

---
+++ tp-hci-g3/src/components/ProfileActions.vue
@@ -0,0 +1,62 @@
+<template>
+    <div class="actions">
+      <button v-for="(action, index) in actions" :key="index" class="action-button">
+        <i :class="action.icon"></i>
+        <span class="label">{{ action.label }}</span>
+        <span class="arrow">&gt;</span>
+      </button>
+    </div>
+  </template>
+  
+  <script setup>
+  const actions = [
+    { label: 'Datos de mi cuenta', icon: 'fas fa-user' },
+    { label: 'Tarjetas', icon: 'fas fa-credit-card' },
+    { label: 'Actividad', icon: 'fas fa-chart-line' },
+    { label: 'Administrar dinero', icon: 'fas fa-wallet' },
+  ]
+  </script>
+  
+  <style scoped>
+  .actions {
+    background-color: #f1f1f1;;
+    border-radius: 10px;
+    padding: 1rem;
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+  }
+  .action-button {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  background: white;
+  padding: 0.75rem 1rem;
+  border-radius: 999px; /* Más redondeado */
+  border: none;
+  font-size: 1.4rem;
+  cursor: pointer;
+  margin-top: 1rem;
+  min-height: 5rem;
+}
+
+.action-button .label {
+  flex-grow: 1; /* Ocupa el espacio disponible */
+  display: flex;
+  align-items: center;
+}
+
+.action-button i {
+  margin-right: 1rem;
+}
+
+.action-button .arrow {
+  font-weight: bold;
+  font-size: 1.6rem;
+}
+  .action-button:hover{
+    background-color: #03192C;
+    color: white;
+  }
+  </style>
+  
---
+++ tp-hci-g3/src/components/ProfileInfo.vue
@@ -0,0 +1,149 @@
+<template>
+    <div class="profile-container">
+      <div class="top-bar">
+        <button class="back-btn"><  Volver</button>
+        <div>
+        <button class="edit-btn">Editar perfil</button>
+        <button class="help-btn">?</button>
+        </div>
+      </div>
+  
+      <div class="profile-box">
+        <div class="left">
+          <img class="avatar" src="/src//assets/images/FotoPerfil.jpeg" alt="Foto de perfil" />
+          <p class="name">Mateo</p>
+          <a href="mailto:mateo.go@gmail.com" class="email">mateo.go@gmail.com</a>
+        </div>
+  
+        <div class="right">
+          <p><strong>DNI:</strong> 40.527.004</p>
+          <p><strong>Nombre:</strong> Mateo Tomás</p>
+          <p><strong>Apellido:</strong> Gorriti</p>
+          <p><strong>Número:</strong> +54 11 7385-4992</p>
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <script setup>
+  // Este componente aún no requiere lógica
+  </script>
+  
+  <style scoped>
+.profile-container {
+  background-color: #03192C;
+  border-radius: 30px;
+  padding: 1.5rem;
+  color: white;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  box-sizing: border-box;
+  min-height: 25rem;
+  /* transition: transform 0.2s ease, box-shadow 0.2s ease; */
+}
+
+/* .profile-container:hover {
+  transform: scale(1.02);
+  box-shadow: 0 4px 12px rgba(0, 0, 0, 0.2);
+} */
+
+.top-bar {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 1rem;
+}
+
+.back-btn{
+  background: none;
+  border: none;
+  color: white;
+  font-weight: bold;
+  cursor: pointer;
+  font-size: 1.3rem;
+}
+.back-btn:hover{
+  text-decoration: underline;
+}
+.edit-btn{
+  background-color: transparent;
+  color: white;
+  border: 2px solid white;
+  border-radius: 999px; 
+  padding: 0.4rem 1rem;
+  font-size: 1.3rem;
+  font-weight: 600;
+  cursor: pointer;
+  transition: background-color 0.3s ease, color 0.3s ease;
+}
+
+.edit-btn:hover {
+  background-color: white;
+  color: #001f3f;
+}
+
+.help-btn{
+  background-color: transparent;
+  color: white;
+  border: 2px solid white;
+  border-radius: 999px; 
+  padding: 0.4rem 1rem;
+  font-size: 1.2rem;
+  font-weight: 600;
+  cursor: pointer;
+  transition: background-color 0.3s ease, color 0.3s ease;
+  margin-left: 1rem;
+}
+
+.help-btn:hover{
+    background-color: white;
+    color: #001f3f; 
+}
+.profile-box {
+  display: flex;
+  justify-content: space-between;
+  gap: 2rem;
+  width: 100%;
+  margin-left: 3rem;
+}
+
+.left {
+  background-color: #073259;
+  padding: 2rem;
+  border-radius: 30px;
+  text-align: center;
+  min-width: 200px;
+  flex-shrink: 0;
+}
+
+.avatar {
+  width: 150px;
+  height: 150px;
+  object-fit: cover;
+  border-radius: 50%;
+  margin-bottom: 1rem;
+  margin-left: 5rem;
+  margin-right: 5rem;
+  margin-top: 2rem;
+}
+.name{
+    font-size: 2rem; 
+}
+.email {
+  color: #9ecbff;
+  text-decoration: none;
+  font-size: 1.2rem;
+}
+
+.right {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  flex-grow: 1;
+  gap: 3rem;
+  font-size: 1.8rem; /* Aumenta el tamaño del texto */
+  font-weight: 500;
+  margin-left: 8rem;
+}
+</style>
